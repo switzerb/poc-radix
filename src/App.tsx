de9950f54729b8c549b2dbd409dc807394a39023
bswitzer <brenna.switzer@gmail.com>
2025-04-07 21:23:12 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <Button>Click me, I'm so interesting.</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/button.stories.tsx
@@ -0,0 +1,30 @@
+import type { Meta, StoryObj } from '@storybook/react';
+ 
+import { Button } from '@/components/ui/button';
+ 
+const meta: Meta<typeof Button> = {
+  component: Button,
+};
+ 
+export default meta;
+type Story = StoryObj<typeof Button>;
+ 
+export const Primary: Story = {
+  args: { 
+    children: 'Primary Button',
+   },
+};
+
+export const Secondary: Story = {
+  args: { 
+    children: 'Secondary Button',
+    variant: 'secondary',
+   },
+};
+
+export const Destructive: Story = {
+  args: { 
+    children: 'Destructive Button',
+    variant: 'destructive',
+   },
+};
